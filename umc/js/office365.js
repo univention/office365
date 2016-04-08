690d98c5198f636ee6b2de427c3ea8ebf9291f8c
Florian Best <best@univention.de>
2016-04-08 10:44:46 +0000

    Bug #41019: fix typo
    svn path=/branches/ucs-4.1/component/univention-office365/; revision=68495

--- umc/js/office365.js
+++ umc/js/office365.js
@@ -387,7 +387,9 @@ define([
 			this._wizard.on('finished', lang.hitch(this, 'closeModule'));
 			this._wizard.on('cancel', lang.hitch(this, 'closeModule'));
 			this.on('close', lang.hitch(this, function() {
-				this._wizard._progressDeferred.reject();
+				if (this._wizard._progressDeferred( {
+					this._wizard._progressDeferred.reject();
+				}
 				if (this._wizard.authorizationWindow) {
 					this._wizard.authorizationWindow.close();
 				}
