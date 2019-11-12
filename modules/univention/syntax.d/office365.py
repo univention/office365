65edf6d6f9cbd278fec86eb73ccae389322c8175
Arvid Requate <requate@univention.de>
2019-11-12 13:00:32 +0100

    Bug #50460: Add univentionOffice365ADConnections syntax and Office365ADConnectionsHook

--- modules/univention/syntax.d/office365.py
+++ modules/univention/syntax.d/office365.py
@@ -1,4 +1,4 @@
-# zarafa4ucsRole UDM syntax
+# Office365 Azure AD Connection UDM syntax
 #
 # Copyright 2012-2019 Univention GmbH
 #
@@ -28,7 +28,9 @@
 # <http://www.gnu.org/licenses/>.
 
 import univention.admin.localization
-from univention.admin.syntax import UDM_Objects
+from univention.admin.syntax import HalfString, complex, UDM_Objects
+import univention.admin.uexceptions
+
 
 translation = univention.admin.localization.translation('univention-admin-handlers-office365')
 _ = translation.translate
@@ -52,3 +54,26 @@ class univentionOffice365ADConnection(UDM_Objects):
 	udm_filter = '(objectClass=univentionOffice365ADConnection)'
 	simple = True
 	regex = None
+
+
+class univentionOffice365ADConnections(complex):
+	"""
+	Syntax for Azure AD connections
+
+	>>> univentionOffice365ADConnections.parse(('defaultADconnection', 'dom1.user1@office365.dev-univention.de'))
+	['defaultADconnection', 'dom1.user1@office365.dev-univention.de']
+	"""
+	subsyntaxes = (
+		(_('Azure AD connection'), univentionOffice365ADConnection),
+		(_('User Principal Name'), HalfString),
+	)
+	subsyntax_names = ('AADConnection', 'userPrincipalName')
+	# subsyntax_key_value = True
+	all_required = True
+	# size = ('Half', 'Half')
+
+	@classmethod
+	def parse(self, texts):
+		p = super(univentionOffice365ADConnections, self).parse(texts)
+		objectID, userPrincipalName = p
+		return p
---
+++ office365_user_ADConnections_hook.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+#
+# Univention Office 365 - UDM hook to access ADConnection data
+#
+# Copyright 2016-2019 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+import json
+import base64
+import zlib
+
+from univention.admin.hook import simpleHook
+from univention.lib.i18n import Translation
+
+_ = Translation('univention-admin-handlers-office365').translate
+
+
+class Office365ADConnectionsHook(simpleHook):
+	type = "Office365ADConnectionsHook"
+
+	@staticmethod
+	def encode_adconnection_data(value):
+		return base64.b64encode(zlib.compress(json.dumps(value)))
+
+	@staticmethod
+	def decode_adconnection_data(value):
+		return json.loads(zlib.decompress(base64.b64decode(value)))
+
+	def hook_open(self, module):
+		if module.get("UniventionOffice365ObjectID"):
+			upn = module.get("UniventionOffice365userPrincipalName", "")
+			value = ["defaultADconnection", upn]
+			module["UniventionOffice365ADConnections"] = [value]
+			return
+		adconnection_data_encoded = module.get("UniventionOffice365Data")
+		if not adconnection_data_encoded:
+			return
+		adconnection_data_encoded = module.get("UniventionOffice365Data")
+		self.adconnection_data = self.decode_adconnection_data(adconnection_data_encoded)
+		module["UniventionOffice365ADConnections"] = []
+		if isinstance(self.adconnection_data, dict):
+			for adconnection, data in self.adconnection_data.iteritems():
+				try:
+					upn = data["userPrincipalName"]
+				except KeyError:
+					upn = ""
+				value = [adconnection, upn]
+				module["UniventionOffice365ADConnections"].append(value)
+
+	def hook_ldap_modlist(self, module, ml=[]):
+		# remove virtual attribute "dummy" from modlist
+		ml = [m for m in ml if m[0] != "dummy"]
+		if module.get("UniventionOffice365ObjectID"):
+			return ml
+		if not module.hasChanged("UniventionOffice365ADConnections"):
+			return ml
+		new_adconnection_data = {}
+		for adconnection, _ in module["UniventionOffice365ADConnections"]:
+			try:
+				new_adconnection_data[adconnection] = self.adconnection_data[adconnection]
+			except KeyError:
+				new_adconnection_data[adconnection] = {}
+		if new_adconnection_data != self.adconnection_data:
+			old = module.get("UniventionOffice365Data")
+			new = self.encode_adconnection_data(new_adconnection_data)
+			ml.append(("univentionOffice365Data", old, new))
+		return ml
