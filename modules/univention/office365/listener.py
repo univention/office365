bf1de79363f78dc0f1119d022453534f9e9fe3f9
Max Christian Pohle <pohle@univention.de>
2021-06-01 14:50:15 +0200

    Bug #48389: Replaced AzureHandler with Graph
    * fixed spelling mistake

--- modules/univention/office365/listener.py
+++ modules/univention/office365/listener.py
@@ -113,7 +113,7 @@ class Office365Listener(object):
 
 		self.not_migrated_to_v3 = self.ucr.is_false('office365/migrate/adconnectionalias')
 
-		self.ah = AzureHandler(self.ucr, name, self.adconnection_alias)
+                self.ah = Graph(self.ucr, name, self.adconnection_alias, logger)
 
 	@property
 	def verified_domains(self):
