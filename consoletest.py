e17aae975efa938654f34d348ccff7504d4051c3
Philipp Hahn <hahn@univention.de>
2021-11-02 20:07:26 +0100

    refactor[py3] Convert print_function
    for Python 3 compatibility

--- consoletest.py
+++ consoletest.py
@@ -30,6 +30,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 import argparse
 import sys
 import pprint
@@ -39,6 +41,48 @@ import base64
 from univention.config_registry import ConfigRegistry
 from univention.office365.azure_handler import AzureHandler
 from univention.office365.azure_auth import AzureAuth, AzureADConnectionHandler
+
+EXAMPLES = """\
+ADD USERS | GROUPS
+------------------
+{0} add users
+
+ADD USERS or GROUPS TO A GROUP
+------------------------------
+{0} -o <objID of target group> add groups <objID of a user or group>  # adding multiple is broken
+
+LIST USERS | GROUPS | DOMAINS | SUBSCRIPTIONS | ADCONNECTION
+------------------------------------------------------
+{0} list users
+{0} list users -s
+{0} list users -c
+{0} list users -f 'accountEnabled eq true'
+{0} list users -s -f "startswith(displayName,'John')"
+{0} list users -s -f "accountEnabled eq true and startswith(displayName,'John')"
+{0} list groups -f "displayName eq 'testgroup01'"
+{0} list domains
+{0} list subscriptions
+{0} list adconnection
+
+MODIFY USERS | GROUPS
+---------------------
+{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 modify users 'displayName=John Doe' 'country=DE' 'accountEnabled=false'
+{0} -o a8fcc4d7-40ca-4648-9593-536e9d73ea77 modify groups "displayName=group9033" "mailNickname=group9033"
+
+DELETE USERS | GROUPS
+---------------------
+{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 delete users
+
+MEMBEROF USERS
+--------------
+{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 memberofgroups users
+{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 memberofobjects users
+
+LICENSES
+--------
+{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 modify licenses add=189a915c-fe4f-4ffa-bde4-85b9628d07a0
+{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 modify licenses remove=189a915c-fe4f-4ffa-bde4-85b9628d07a0
+""".format(sys.argv[0])
 
 
 def print_users(users, complete=False, short=False):
@@ -143,45 +187,7 @@ if __name__ == "__main__":
 
 	if args.object in ["users", "groups", "licenses", "domains", "subscriptions", "adconnection"]:
 		if args.action == "examples":
-			print "ADD USERS | GROUPS"
-			print "------------------"
-			print "{0} add users".format(sys.argv[0])
-			print ""
-			print "ADD USERS or GROUPS TO A GROUP"
-			print "------------------------------"
-			print "{0} -o <objID of target group> add groups <objID of a user or group>  # adding multiple is broken".format(sys.argv[0])
-			print ""
-			print "LIST USERS | GROUPS | DOMAINS | SUBSCRIPTIONS | ADCONNECTION"
-			print "------------------------------------------------------"
-			print "{0} list users".format(sys.argv[0])
-			print "{0} list users -s".format(sys.argv[0])
-			print "{0} list users -c".format(sys.argv[0])
-			print "{0} list users -f 'accountEnabled eq true'".format(sys.argv[0])
-			print "{0} list users -s -f \"startswith(displayName,'John')\"".format(sys.argv[0])
-			print "{0} list users -s -f \"accountEnabled eq true and startswith(displayName,'John')\"".format(sys.argv[0])
-			print "{0} list groups -f \"displayName eq 'testgroup01'\"".format(sys.argv[0])
-			print "{0} list domains".format(sys.argv[0])
-			print "{0} list subscriptions".format(sys.argv[0])
-			print "{0} list adconnection".format(sys.argv[0])
-			print ""
-			print "MODIFY USERS | GROUPS"
-			print "---------------------"
-			print "{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 modify users 'displayName=John Doe' 'country=DE' 'accountEnabled=false'".format(sys.argv[0])
-			print '{0} -o a8fcc4d7-40ca-4648-9593-536e9d73ea77 modify groups "displayName=group9033" "mailNickname=group9033"'.format(sys.argv[0])
-			print ""
-			print "DELETE USERS | GROUPS"
-			print "---------------------"
-			print "{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 delete users".format(sys.argv[0])
-			print ""
-			print "MEMBEROF USERS"
-			print "--------------"
-			print "{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 memberofgroups users".format(sys.argv[0])
-			print "{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 memberofobjects users".format(sys.argv[0])
-			print ""
-			print "LICENSES"
-			print "--------"
-			print "{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 modify licenses add=189a915c-fe4f-4ffa-bde4-85b9628d07a0".format(sys.argv[0])
-			print "{0} -o bd5ea47e-cc70-4c5e-9c66-b6a07695b7d1 modify licenses remove=189a915c-fe4f-4ffa-bde4-85b9628d07a0".format(sys.argv[0])
+			print(EXAMPLES)
 			sys.exit(0)
 		elif args.action == "list":
 			# see below
@@ -201,7 +207,7 @@ if __name__ == "__main__":
 				parser.error("Please supply the objectIDs of users or groups to add to the group.")
 		else:
 			name = "name{0}".format(random.randint(1000, 9999))
-			print "adding {0} with random name '{1}'...".format(args.object, name)
+			print("adding {0} with random name '{1}'...".format(args.object, name))
 			if args.object == "users":
 				attributes = {
 					"accountEnabled": True,
@@ -220,9 +226,9 @@ if __name__ == "__main__":
 				new_group = ah.list_groups(ofilter="displayName eq '{}'".format(name))
 				print_groups(new_group, args.complete, args.short)
 			else:
-				print "other object types not yet implemented"
+				print("other object types not yet implemented")
 	elif args.action == "list":
-		print "listing {0}: {1}...".format(args.object, args.objectid if args.objectid else "all")
+		print("listing {0}: {1}...".format(args.object, args.objectid if args.objectid else "all"))
 		if args.object == "users":
 			users = ah.list_users(args.objectid, args.filter)
 			print_users(users, args.complete, args.short)
@@ -234,14 +240,14 @@ if __name__ == "__main__":
 			else:
 				groups = groups["value"]
 			for group_id in [g["objectId"] for g in groups]:
-				print "MEMBERS OF %r:" % group_id
+				print("MEMBERS OF %r:" % group_id)
 				members = ah.get_groups_direct_members(group_id)
 				if "value" in members:
-					print "\n".join([m["url"] for m in members["value"]]) if members["value"] else "None."
+					print("\n".join([m["url"] for m in members["value"]]) if members["value"] else "None.")
 				else:
-					print "Error retrieving group members."
-					print members
-				print ""
+					print("Error retrieving group members.")
+					print(members)
+				print("")
 		elif args.object == "subscriptions":
 			subscriptions = ah.list_subscriptions()
 			pprint.pprint(subscriptions)
@@ -252,7 +258,7 @@ if __name__ == "__main__":
 			adconnection = ah.list_adconnection_details()
 			pprint.pprint(adconnection)
 		else:
-			print "object type '{}' not yet implemented".format(args.object)
+			print("object type '{}' not yet implemented".format(args.object))
 	elif args.action == "modify":
 		if args.object not in ["users", "groups", "licenses"]:
 			parser.error('Currently only object types "users", "groups" and "licenses" supported.')
@@ -263,7 +269,7 @@ if __name__ == "__main__":
 				modifications[key] = value
 			except ValueError:
 				parser.error(u"Argument '{0}' is not of form KEY=VALUE.".format(kv))
-		print u"modifying {}: {} attributes: {}...".format(args.object[:-1], args.objectid, modifications)
+		print(u"modifying {}: {} attributes: {}...".format(args.object[:-1], args.objectid, modifications))
 		if args.object == "users":
 			ah.modify_user(args.objectid, modifications)
 		elif args.object == "groups":
@@ -281,23 +287,23 @@ if __name__ == "__main__":
 	elif args.action == "delete":
 		if args.object not in ["users", "groups"]:
 			parser.error('Currently only object types "users" and "groups" supported.')
-		print "deleting {} {}...".format(args.object[:-1], args.objectid)
+		print("deleting {} {}...".format(args.object[:-1], args.objectid))
 		if args.object == "users":
 			ah.delete_user(args.objectid)
 		else:
 			ah.delete_group(args.objectid)
 	elif args.action in ["memberofgroups", "memberofobjects"]:
-		print "querying {0} of {1}...".format(args.action[8:], args.objectid)
+		print("querying {0} of {1}...".format(args.action[8:], args.objectid))
 		member_in = member_of(args.action[8:], args.objectid)
-		print "member_in: {}".format(pprint.pformat(member_in))
-		print "{0} is member of the following {1}:".format(args.objectid, args.action[8:])
-		print "{}".format(member_in["value"])
+		print("member_in: {}".format(pprint.pformat(member_in)))
+		print("{0} is member of the following {1}:".format(args.objectid, args.action[8:]))
+		print("{}".format(member_in["value"]))
 		if member_in["value"]:
-			print "resolving object IDs..."
+			print("resolving object IDs...")
 			objects = ah.resolve_object_ids(member_in["value"])
-			print "objects: {}".format(pprint.pformat(objects))
+			print("objects: {}".format(pprint.pformat(objects)))
 			for obj in objects["value"]:
-				print "objectId {}:".format(obj["objectId"])
+				print("objectId {}:".format(obj["objectId"]))
 				pprint.pprint(obj)
 
 	else:
