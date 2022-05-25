1a479eb2808f7032dd8cd96d30d8f7050642b645
Iván Barbecho Delgado <delgado.extern@univention.de>
2022-05-25 10:53:01 +0200

    Bug #51940: restructuring folders

--- modules/univention/office365/utils/utils.py
+++ modules/univention/office365/utils/utils.py
@@ -1,9 +1,26 @@
 import base64
 import subprocess
+import random
+import string
 
 from univention.office365.logging2udebug import get_logger
 
 logger = get_logger("office365", "o365")
+
+
+def create_random_pw():
+	# have at least one char from each category in password
+	# https://msdn.microsoft.com/en-us/library/azure/jj943764.aspx
+	pw = list(random.choice(string.ascii_lowercase))
+	pw.append(random.choice(string.ascii_uppercase))
+	pw.append(random.choice(string.digits))
+	pw.append(random.choice(u"@#$%^&*-_+=[]{}|\\:,.?/`~();"))
+	pw.extend(random.choice(string.ascii_letters + string.digits + u"@#$%^&*-_+=[]{}|\\:,.?/`~();") for _ in range(12))
+	random.shuffle(pw)
+	return u"".join(pw)
+
+
+_default_azure_service_plan_names = "SHAREPOINTWAC, SHAREPOINTWAC_DEVELOPER, OFFICESUBSCRIPTION, OFFICEMOBILE_SUBSCRIPTION, SHAREPOINTWAC_EDU"
 
 
 def listener_restart():
