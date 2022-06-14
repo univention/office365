9f8436c472450e26e5048c380db9215378c8f023
Iván Barbecho Delgado <delgado.extern@univention.de>
2022-06-14 13:01:39 +0200

    Bug #51940: Update copyright headers for scripts files

--- scripts/ucs-test/consoletest.py
+++ scripts/ucs-test/consoletest.py
@@ -3,7 +3,7 @@
 #
 # Univention Office 365 - cmdline tests
 #
-# Copyright 2016-2021 Univention GmbH
+# Copyright 2016-2022 Univention GmbH
 #
 # http://www.univention.de/
 #
--- scripts/ucs-test/terminaltest.py
+++ scripts/ucs-test/terminaltest.py
@@ -3,7 +3,7 @@
 #
 # Univention Microsoft 365 - cmdline microsoft graph tests
 #
-# Copyright 2016-2021 Univention GmbH
+# Copyright 2016-2022 Univention GmbH
 #
 # http://www.univention.de/
 #
